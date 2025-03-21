40a5d28faa4d3cffa352509c0add26216db769d3
gerardoqro <31111873+gerardoqro@users.noreply.github.com>
2025-03-20 18:54:03 -0600

    20 marzo

--- js/init.js
+++ js/init.js
@@ -1,3 +1,22 @@
+function videoG() {
+	let person = prompt("Please enter your name", "Harry Potter");
+	if (person != null) {
+	  document.getElementById("demo").innerHTML =
+	  "Hello " + person + "! How are you today?";
+	}
+  }
+
+
+function playVideo(miv) {
+	var myVideo = document.getElementById(miv); 
+ 	myVideo.play(); 
+} 
+
+function pauseVideo(miv) { 
+	var myVideo = document.getElementById(miv);
+	myVideo.pause(); 
+} 
+
 $(function(){
 	$(".thumbnails a").attr('rel', 'gallery').fancybox();
 
